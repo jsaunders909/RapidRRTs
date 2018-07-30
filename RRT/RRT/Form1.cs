99c20db73595ce63b0b6dab2fbe997d2e65cb3a1
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2018-07-30 22:18:44 +0100

    Add files via upload

---
+++ RRT/RRT/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RRT
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker b;
+        Bitmap bmp;
+        RapidRT rrt;
+        Coord from;
+        Coord to;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Width = 500;
+            rrt = new RapidRT(500, 500, 5,new Coord(250, 250));
+            b = new BackgroundWorker();
+            b.WorkerReportsProgress = true;
+            b.DoWork += B_DoWork;
+            b.ProgressChanged += B_ProgressChanged;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            b.RunWorkerAsync();
+        }
+
+        private void B_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DrawLine(from, to);
+            pictureBox1.Image = bmp;
+        }
+
+        private void B_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+                rrt.RunIteration();
+                from = rrt.mostRecentEdge.Item1;
+                to = rrt.mostRecentEdge.Item2;
+                b.ReportProgress(i);
+                Thread.Sleep(30);
+            }
+        }
+
+        private void DrawLine(Coord from, Coord to)
+        {
+            double distance = rrt.GetDistance(from, to);
+            double xStep = (to.X - from.X) / distance;
+            double yStep = (to.Y - from.Y) / distance;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                for (int i = 0; i < distance; i++)
+                {
+                    int x = (int)(from.X + (i * xStep));
+                    int y = (int)(from.Y + (i * yStep));
+                    bmp.SetPixel(x, y, Color.Black);
+                }
+            }
+        }
+    }
+}
---
+++ RRT/RRT/RapidRT.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRT
+{
+    public struct Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class RapidRT
+    {
+        List<Coord> seenList;
+        public Tuple<Coord, Coord> mostRecentEdge;
+        int width;
+        int length;
+        Random random;
+        int stepSize;
+
+        public RapidRT(int width, int length, int stepSize, Coord startPoint)
+        {
+            this.width = width;
+            this.length = length;
+            seenList = new List<Coord>() { startPoint };
+            random = new Random();
+            this.stepSize = stepSize;
+        }
+
+        private Coord GetRandomCoord()
+        {
+            int x = random.Next(0, width);
+            int y = random.Next(0, length);
+            return new Coord(x, y);
+        }
+        public double GetDistance(Coord coord1, Coord coord2)
+        {
+            int x = coord1.X - coord2.X;
+            int y = coord1.Y - coord2.Y;
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        private Coord GetNearest(Coord coord)
+        {
+            double mindistance = double.PositiveInfinity;
+            Coord bestCoord = seenList[seenList.Count - 1];
+            foreach (Coord c in seenList)
+            {
+                double distance = GetDistance(c, coord);
+                if (distance < mindistance && distance != 0)
+                {
+                    mindistance = distance;
+                    bestCoord = c;
+                }
+            }
+            return bestCoord; 
+        }
+
+        private Coord StepInDirection(Coord nearestCoord, Coord randomCoord)
+        {
+            int x = randomCoord.X - nearestCoord.X;
+            int y = randomCoord.Y - nearestCoord.Y;
+
+            double distance = GetDistance(randomCoord, nearestCoord);
+            double stepFactor = stepSize / distance;
+
+            return new Coord((int)(nearestCoord.X + (x * stepFactor)), (int)(nearestCoord.Y + (y * stepFactor)));
+        }
+
+        public void RunIteration()
+        {
+            Coord randomCoord = GetRandomCoord();
+            Coord nearestCoord = GetNearest(randomCoord);
+            Coord newCoord = StepInDirection(nearestCoord, randomCoord);
+            mostRecentEdge =  new Tuple<Coord, Coord>(newCoord, nearestCoord);
+            if (newCoord.X < -1000)
+            {
+                newCoord.X++;
+            }
+            seenList.Add(newCoord);
+        }
+    }
+}
